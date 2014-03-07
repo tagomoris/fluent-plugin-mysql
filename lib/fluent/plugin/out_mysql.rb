4d7ac3dc526a988ef5166233022200ef4992c36c
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-03-07 20:51:01 +0900

    supports log_level of Fluentd v0.10.43

--- lib/fluent/plugin/out_mysql.rb
+++ lib/fluent/plugin/out_mysql.rb
@@ -25,6 +25,11 @@ class Fluent::MysqlOutput < Fluent::BufferedOutput
     require 'jsonpath'
   end
 
+  # Define `log` method for v0.10.42 or earlier
+  unless method_defined?(:log)
+    define_method("log") { $log }
+  end
+
   def configure(conf)
     super
 
